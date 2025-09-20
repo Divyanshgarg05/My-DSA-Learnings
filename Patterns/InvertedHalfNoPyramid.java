96455323306b79df9344a80d7596312abf019827
Divyansh Garg <divyanshgarg844@gmail.com>
2025-09-20 22:27:42 +0530

    Added new pattern programs

--- Patterns/InvertedHalfNoPyramid.java
+++ Patterns/InvertedHalfNoPyramid.java
@@ -1,10 +1,10 @@
 package Patterns;
 
-public class HalfPyramid {
+public class InvertedHalfNoPyramid {
     public static void main(String[] args) {
-        for(int i =1;i<=4;i++){
+        for(int i=5;i>=0;i--){
             for(int j=1;j<=i;j++){
-                System.out.print(j +" ");
+                System.out.print(j);
             }
             System.out.println();
         }
