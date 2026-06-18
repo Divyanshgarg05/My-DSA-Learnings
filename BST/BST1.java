da7cf0658ea67d04e88bac2361fa59d8c0f0cae8
Divyansh Garg <divyanshgarg844@gmail.com>
2026-06-19 05:25:25 +0530

    Completed trees

--- BST/BST1.java
+++ BST/BST1.java
@@ -156,8 +156,21 @@ public class BST1 {
         preOrder(root.left);
         preOrder(root.right);
     }
+
+    public static Node createBST(int arr[],int st,int end){
+        if(st > end){
+            return null;
+        }
+        int mid = (st+end)/2;
+        Node root = new Node(arr[mid]);
+        root.left = createBST(arr, st, mid-1);
+        root.right = createBST(arr, mid+1, end);
+
+        return root;
+    }
     public static void main(String[] args) {
         int values[] = {8,5,3,1,4,6,10,11,14};
+        int arr[]= {3,5,6,8,10,11,12};
         Node root = null;
 
         for(int i=0;i<values.length;i++){
